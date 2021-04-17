07c7483740c9b3d1a78a13fc63090b8c415e0058
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-04-17 18:21:30 +0600

    Missing person added with problem

--- app/src/main/java/com/example/bdosn_app/AddMissingPerson.java
+++ app/src/main/java/com/example/bdosn_app/AddMissingPerson.java
@@ -1,13 +1,156 @@
 package com.example.bdosn_app;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.InputStream;
+import java.util.Random;
+import java.util.UUID;
+
 public class AddMissingPerson extends AppCompatActivity {
+    private ImageView missingPhoto;
+    Uri imageUri;
+    private FirebaseStorage storage;
+    EditText name, location, age, height, gender, relation, description, contact, lastSeen;
+    Button confirm;
+    Bitmap bitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_missing_person);
+        missingPhoto = findViewById(R.id.missing_photo);
+        confirm = findViewById(R.id.confirm_button);
+
+        missingPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choosePicture();
+            }
+        });
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadInformation();
+            }
+        });
+    }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(imageUri);
+                bitmap = BitmapFactory.decodeStream(inputStream);
+                missingPhoto.setImageBitmap(bitmap);
+            } catch (Exception e) {
+
+            }
+            //  uploadPicture();
+        }
+    }
+
+    private void uploadInformation() {
+
+        name =(EditText) findViewById(R.id.missing_name_text);
+        location =(EditText) findViewById(R.id.missing_location_text);
+        age =(EditText) findViewById(R.id.age_text);
+        height = (EditText)findViewById(R.id.height_text);
+        gender =(EditText) findViewById(R.id.gender_text);
+        relation =(EditText) findViewById(R.id.relation_text);
+        description =(EditText) findViewById(R.id.general_desc_text);
+        contact =(EditText) findViewById(R.id.contact_text);
+        lastSeen =(EditText) findViewById(R.id.last_seen_text);
+
+
+        final String randomKey = UUID.randomUUID().toString();
+        storage=FirebaseStorage.getInstance();
+        StorageReference riversRef = storage.getReference("image" + randomKey);
+
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setTitle("Uploading Information...");
+        pd.show();
+
+        riversRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        riversRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                pd.dismiss();
+
+                                FirebaseDatabase db=FirebaseDatabase.getInstance();
+                                DatabaseReference root=db.getReference("MissingPersons");
+
+                                MissingPerson missingPerson=new MissingPerson(age.getText().toString(),contact.getText().toString(),
+                                        description.getText().toString(),gender.getText().toString(),height.getText().toString(),
+                                        uri.toString(),lastSeen.getText().toString(),location.getText().toString(),name.getText().toString(),
+                                        relation.getText().toString());
+                                root.child(contact.getText().toString()).setValue(missingPerson);
+
+                                name.setText("");
+                                age.setText("");
+                                height.setText("");
+                                location.setText("");
+                                lastSeen.setText("");
+                                relation.setText("");
+                                gender.setText("");
+                                description.setText("");
+                                contact.setText("");
+                                missingPhoto.setImageResource(R.drawable.profile_icon);
+                                Toast.makeText(getApplicationContext(), "Uploaded Successfully", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        pd.dismiss();
+                        Toast.makeText(getApplicationContext(), "Failed to Upload", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                        double progressPercent = (100.00 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                        pd.setMessage("Progress: " + (int) progressPercent + "%");
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/example/bdosn_app/MissingPerson.java
@@ -0,0 +1,98 @@
+package com.example.bdosn_app;
+
+public class MissingPerson {
+    String age,contact,desc,gender,height,image,last_seen,location,name,relation;
+
+    public MissingPerson(String age, String contact, String desc, String gender, String height, String image, String last_seen, String location, String name, String relation) {
+        this.age = age;
+        this.contact = contact;
+        this.desc = desc;
+        this.gender = gender;
+        this.height = height;
+        this.image = image;
+        this.last_seen = last_seen;
+        this.location = location;
+        this.name = name;
+        this.relation = relation;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getLast_seen() {
+        return last_seen;
+    }
+
+    public void setLast_seen(String last_seen) {
+        this.last_seen = last_seen;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRelation() {
+        return relation;
+    }
+
+    public void setRelation(String relation) {
+        this.relation = relation;
+    }
+}
